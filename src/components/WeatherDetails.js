c2fc2cc314a6ddb5f9dbad555bc2b96a5d270cb4
Liliana Aqueveque <lili.aqueveque@gmail.com>
2024-01-10 18:29:26 -0300

    info personalized to city, fetching is ready

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,20 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function WeatherDetails() {
+function WeatherDetails(
+  {temp,
+  humidity,
+  pressure,
+  weatherType,
+  name,
+  speed,
+  country,
+  sunset,}
+) {
+    const [weatherState, setWeatherState] = useState('');
+
   return (
     <div>
-        <article className='widget'>
-            <div className='weatherIcon'>
-                <i className="wi wi-day-sunny"></i>
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className="wi wi-day-sunny"></i>
+        </div>
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>{temp}&deg;</span>
+          </div>
+          <div className="description">
+            <div className="weatherCondition">{weatherType}</div>
+            <div className="place">{name}, {country}</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {sunset} PM <br />
+                Sunset
+              </p>
             </div>
-            <div className='weatherInfo'>
-                <div className='temperature'>
-                    <span>23.99&deg;</span>
-                </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {humidity}
+                <br />
+                Humidity
+              </p>
             </div>
-        </article>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {pressure}
+                <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {speed}
+                <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
     </div>
-  )
+  );
 }
 
-export default WeatherDetails
+export default WeatherDetails;
--- src/components/searchMain.js
+++ src/components/searchMain.js
@@ -1,22 +1,67 @@
-import React, { useState } from 'react';
-import '../components/style.css';
-import WeatherDetails from './WeatherDetails';
+import React, { useEffect, useState } from "react";
+import "../components/style.css";
+import WeatherDetails from "./WeatherDetails";
 
 function SearchMain() {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [tempInfo, setTempInfo] = useState({});
 
-    return (
-        <>
-        <div className='wrap'>
-            <div className='searc'>
-                <input type='search' placeholder='Search City...' id='search' value={searchTerm} onChange={(e) => 
-                setSearchTerm(e.target.value)} />
-            </div>
-            <button className='searchButton'>Search</button>
+  const getWeatherInfo = async () => {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=0f93d8766a9ab266a131394edae0bb41`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+
+      setTempInfo(myNewWeatherInfo);
+      //console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    //function
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search City..."
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
-        <WeatherDetails />
-        </>
-    );
+        <button className="searchButton" onClick={getWeatherInfo}>
+          Search
+        </button>
+      </div>
+      {/*This is the weather details page */}
+      <WeatherDetails {...tempInfo} />
+    </>
+  );
 }
 
 export default SearchMain;
